8dc59d7568c275e0d70c07dbb0bc9fa3ba0cd4ed
Alex Bednarczyk <alex.bednarczyk@gmail.com>
2022-02-15 07:09:43 -0600

    feat: add spinner command (#3)
    * feat: add spinner command
    
    * fix: lint issue with ReadBytes

--- cmd/root.go
+++ cmd/root.go
@@ -5,14 +5,12 @@ import (
 	"io/ioutil"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var (
 	version     string
-	cfgFile     string
 	cfgFileName = ".gopher"
 )
 
@@ -40,41 +38,27 @@ func er(msg interface{}) {
 }
 
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
+	// Search config in present working directory with name ".gopher" (without extension).
+	viper.AddConfigPath("./")
+	viper.SetConfigName(cfgFileName)
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found
+			initConfigPath := "./" + string(os.PathSeparator) + cfgFileName + ".yaml"
+			if err := ioutil.WriteFile(initConfigPath, []byte(""), 0666); err != nil {
+				fmt.Println("error:", err)
+			}
+			// Set defaults for config file
+			viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+			viper.SetDefault("license", "apache")
+			fmt.Printf("Config file initialized: %s\n", initConfigPath)
+		} else {
+			// Config file was found but another error was produced
 			er(err)
 		}
-
-		// Search config in home directory with name ".gopher" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(cfgFileName)
-		viper.SetConfigType("yaml")
-
-		if err := viper.ReadInConfig(); err != nil {
-			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-				// Config file not found; ignore error if desired
-				initConfigPath := home + string(os.PathSeparator) + cfgFileName + ".yaml"
-				if err := ioutil.WriteFile(initConfigPath, []byte(""), 0666); err != nil {
-					fmt.Println("error:", err)
-				}
-				fmt.Printf("Config file initialized: %s\n", initConfigPath)
-			} else {
-				// Config file was found but another error was produced
-				er(err)
-			}
-
-		}
-
 	}
-
-	// Set defaults for config file
-	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-	viper.SetDefault("license", "apache")
 
 	if err := viper.WriteConfig(); err != nil {
 		fmt.Println("error:", err)
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/alexbednarczyk/cli-gopher
 go 1.14
 
 require (
+	github.com/briandowns/spinner v1.18.0
 	github.com/debber/debber-v0.3 v0.0.0-20201001103732-e53137b65bb8 // indirect
+	github.com/fsnotify/fsnotify v1.4.7
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/laher/argo v0.0.0-20140722103944-11d91c83cc0f // indirect
 	github.com/laher/goxc v0.18.1 // indirect
